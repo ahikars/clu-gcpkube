ebd5c4adf09c78db17422ac4e6a1ba07f36884af
ahikar <ahikars@gmail.com>
2020-07-08 02:27:29 +0300

    oauth2

--- gke.tf
+++ gke.tf
@@ -33,6 +33,24 @@ variable "gke_num_nodes" {
   description = "number of gke nodes"
 }
 
+data "google_client_config" "default" {
+}
+
+data "google_container_cluster" "my_cluster" {
+  name = "my-cluster"
+  zone = "us-central1-a"
+}
+
+provider "kubernetes" {
+  load_config_file = false
+
+  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  token = data.google_client_config.default.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
+  )
+}
+
 # GKE cluster
 resource "google_container_cluster" "primary" {
   name     = "${var.project_id}-gke"
