6d5c5769cfb8d2cb04e24af202ff5dd0164dee5d
ahikar <ahikars@gmail.com>
2020-07-08 02:35:48 +0300

    "

--- gke.tf
+++ gke.tf
@@ -37,7 +37,7 @@ data "google_client_config" "default" {
 }
 
 data "google_container_cluster" "my_cluster" {
-  name = "${var.project_id}-gke
+  name = "${var.project_id}-gke"
   zone = "us-central1-a"
 }
 
