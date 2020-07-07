48ec9d27d4619211b6e9c597d48c697b3163939d
ahikar <ahikars@gmail.com>
2020-07-08 02:57:18 +0300

    aa

--- gke.tf
+++ gke.tf
@@ -53,12 +53,13 @@ resource "google_container_cluster" "primary" {
     }
     
   }
+  node_config {
   oauth_scopes = [
       "https://www.googleapis.com/auth/logging.write",
       "https://www.googleapis.com/auth/monitoring",
       "https://www.googleapis.com/auth/cloud-platform",
     ]
-
+  }
 }
  
 # Separately Managed Node Pool
