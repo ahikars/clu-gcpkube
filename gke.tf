cf6e80211d61af4253841cc009ba995b2b3ebcdf
ahikar <ahikars@gmail.com>
2020-07-08 02:53:38 +0300

    aa

--- gke.tf
+++ gke.tf
@@ -53,6 +53,11 @@ resource "google_container_cluster" "primary" {
     }
     
   }
+  oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
 
 }
  
